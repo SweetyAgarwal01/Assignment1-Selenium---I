bac7557e8b4c5527b64cdc7c3d1dcd78a1394659
SweetyAgarwal01 <45209773+SweetyAgarwal01@users.noreply.github.com>
2018-11-27 23:52:13 +0530

    Add files via upload

---
+++ SeleniumBrowser.java
@@ -0,0 +1,18 @@
+package com.ibm.seleniumconcepts;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver = new FirefoxDriver();
+
+		//System.out.println("Hello");
+		}
+	
+
+}
